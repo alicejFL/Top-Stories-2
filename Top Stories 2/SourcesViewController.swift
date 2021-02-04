7f65c69f675a67e60ebc31390c02e56e00ea38fb
Alice <alicej@psdschools.org>
2021-02-04 10:15:53 -0700

    Set API Query

--- Top Stories 2/SourcesViewController.swift
+++ Top Stories 2/SourcesViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class SourcesViewController: UITableViewController {
-
-    override func viewDidLoad() {
+class SourcesViewController: UITableViewController
+{
+    var sources = [[String:String]]()
+    let apiKey = "5d892509a49046a087917c466fa80d09"
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "News Sources"
+        _ =  "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        
     }
 
 
